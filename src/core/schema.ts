2ddda036f5b3fdb1652402ec1a63b75c2feb7db8
dejavuurbe <dejavuurbe@gmail.com>
2026-09-23 08:06:43 -0300

    Strengthen reusable author and work semantic relationships

--- src/core/schema.ts
+++ src/core/schema.ts
@@ -1,14 +1,19 @@
 import { site } from '../content/site';
 
+const validExternalUrls = (urls: string[]) =>
+  urls.filter((url) => url && url !== '#' && /^https?:\/\//.test(url));
+
 export function personSchema() {
   return {
     '@context': 'https://schema.org',
     '@type': 'Person',
-    name: site.name,
+    '@id': `${site.url}#author`,
+    name: site.canonicalName || site.name,
+    alternateName: site.searchVariants.length ? site.searchVariants : undefined,
     url: site.url,
     jobTitle: site.role,
     description: site.description,
-    sameAs: site.social.map((item) => item.url).filter((url) => url !== '#'),
+    sameAs: validExternalUrls(site.social.map((item) => item.url)),
   };
 }
 
@@ -17,11 +22,16 @@ export function bookSchema() {
   return {
     '@context': 'https://schema.org',
     '@type': 'Book',
+    '@id': `${site.url}#${book.id}`,
     name: book.title,
-    author: { '@type': 'Person', name: site.name },
-    isbn: book.isbn,
-    datePublished: book.year,
+    author: { '@id': `${site.url}#author` },
+    isbn: book.isbn || undefined,
+    datePublished: book.year || undefined,
+    bookEdition: book.publisher || undefined,
+    numberOfPages: book.pages ? Number(book.pages) || undefined : undefined,
+    genre: book.genre || undefined,
     description: book.synopsis,
+    url: site.url,
   };
 }
 
